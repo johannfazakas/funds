5b3104f1499d2b11b86a62b81b20e0b202b756de
Johann Fazakas <johannfazakas@gmail.com>
2024-10-09 17:12:09 +0300

    parsed wallet fund name on simple import items

--- service/import-service/src/main/kotlin/ro/jf/funds/importer/service/domain/service/parser/WalletCsvImportParser.kt
+++ service/import-service/src/main/kotlin/ro/jf/funds/importer/service/domain/service/parser/WalletCsvImportParser.kt
@@ -4,13 +4,15 @@ import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.format.FormatStringsInDatetimeFormats
 import kotlinx.datetime.format.byUnicodePattern
 import ro.jf.funds.importer.service.domain.exception.ImportDataException
+import ro.jf.funds.importer.service.domain.model.FundMatcher
 import ro.jf.funds.importer.service.domain.model.ImportConfiguration
 import ro.jf.funds.importer.service.domain.model.ImportRecord
 import ro.jf.funds.importer.service.domain.model.ImportTransaction
 
-private const val ACCOUNT_LABEL_COLUMN = "account"
+private const val ACCOUNT_NAME_COLUMN = "account"
 private const val AMOUNT_COLUMN = "amount"
 private const val CURRENCY_COLUMN = "currency"
+private const val LABEL_COLUMN = "labels"
 private const val DATE_COLUMN = "date"
 private const val DATE_FORMAT = "yyyy-MM-dd HH:mm:ss"
 private const val NOTE_COLUMN = "note"
@@ -53,14 +55,32 @@ class WalletCsvImportParser(
         return ImportTransaction(
             transactionId = transactionId,
             date = csvRows.first().getDateTime(DATE_COLUMN, dateTimeFormat),
-            records = csvRows.map { csvRow ->
-                ImportRecord(
-                    accountName = csvRow.getString(ACCOUNT_LABEL_COLUMN)
-                        .let { importConfiguration.accountMatchers.getAccountName(it) },
-                    currency = csvRow.getString(CURRENCY_COLUMN),
-                    amount = csvRow.getBigDecimal(AMOUNT_COLUMN)
+            records = csvRows.flatMap { csvRow -> toImportRecords(importConfiguration, csvRow) }
+        )
+    }
+
+    private fun toImportRecords(importConfiguration: ImportConfiguration, csvRow: CsvRow): List<ImportRecord> {
+        val importAccountName = csvRow.getString(ACCOUNT_NAME_COLUMN)
+        val accountName = importConfiguration.accountMatchers.getAccountName(importAccountName)
+        val importType = csvRow.getString(LABEL_COLUMN)
+        val currency = csvRow.getString(CURRENCY_COLUMN)
+        val amount = csvRow.getBigDecimal(AMOUNT_COLUMN)
+        val fundMatcher = importConfiguration.fundMatchers.getFundMatcher(importAccountName, importType)
+
+        return when (fundMatcher) {
+            is FundMatcher.ByLabel ->
+                listOf(ImportRecord(accountName, fundMatcher.fundName, currency, amount))
+
+            is FundMatcher.ByAccountLabel ->
+                listOf(ImportRecord(accountName, fundMatcher.fundName, currency, amount))
+
+            is FundMatcher.ByAccountLabelWithTransfer -> {
+                listOf(
+                    ImportRecord(accountName, fundMatcher.initialFundName, currency, amount),
+                    ImportRecord(accountName, fundMatcher.initialFundName, currency, amount.negate()),
+                    ImportRecord(accountName, fundMatcher.destinationFundName, currency, amount)
                 )
             }
-        )
+        }
     }
 }
