893b25659dc46a9543726388d40dd117cbff22f5
Johann Fazakas <johannfazakas@gmail.com>
2024-11-22 09:27:01 +0200

    extracted import fund transaction mapping in converter implementations

--- service/import/import-service/src/main/kotlin/ro/jf/funds/importer/service/service/conversion/converter/ImportFundConverterUtils.kt
+++ service/import/import-service/src/main/kotlin/ro/jf/funds/importer/service/service/conversion/converter/ImportFundConverterUtils.kt
@@ -1,11 +1,13 @@
 package ro.jf.funds.importer.service.service.conversion.converter
 
+import kotlinx.datetime.LocalDate
 import ro.jf.funds.account.api.model.AccountTO
 import ro.jf.funds.commons.model.Currency
 import ro.jf.funds.importer.service.domain.ImportParsedRecord
 import ro.jf.funds.importer.service.domain.ImportParsedTransaction
-import ro.jf.funds.importer.service.service.conversion.ImportFundConversionService.ConversionRequest
-import ro.jf.funds.importer.service.service.conversion.ImportFundConversionService.CurrencyPair
+import ro.jf.funds.importer.service.service.conversion.ImportFundConversionService.*
+import java.math.BigDecimal
+import java.util.*
 
 fun ImportParsedTransaction.getRequiredImportConversions(
     resolveAccount: ImportParsedRecord.() -> AccountTO
@@ -22,3 +24,25 @@ fun ImportParsedTransaction.getRequiredImportConversions(
             )
         )
     }
+
+fun ImportParsedRecord.toImportCurrencyFundRecord(
+    date: LocalDate,
+    fundId: UUID,
+    account: AccountTO,
+    resolveConversionRate: ConversionRequest.() -> BigDecimal
+): ImportFundRecord {
+    return ImportFundRecord(
+        fundId = fundId,
+        accountId = account.id,
+        amount = if (unit == account.unit) {
+            amount
+        } else {
+            val conversionRate = ConversionRequest(
+                date,
+                CurrencyPair(unit as Currency, account.unit as Currency)
+            ).resolveConversionRate()
+            conversionRate * amount
+        },
+        unit = account.unit as Currency,
+    )
+}
