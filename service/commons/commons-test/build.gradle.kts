7eeb5b233c3e07f4a4f32a0ada912b30920a69ef
Johann Fazakas <johannfazakas@gmail.com>
2025-02-19 09:50:25 +0200

    removed unnecessary commons dependencies

--- service/commons/commons-test/build.gradle.kts
+++ service/commons/commons-test/build.gradle.kts
@@ -5,24 +5,13 @@ plugins {
 dependencies {
     implementation(project(":commons:commons"))
 
-    // TODO(Johann) a lot of these deps are not required anymore as commons-api was added
-    implementation(libs.ktor.client.content.negotiation)
-    implementation(libs.ktor.serialization.kotlinx.json)
-    implementation(libs.kotlinx.serialization.json)
-
     api(libs.kotlin.test.junit)
     api(libs.junit.jupiter)
-    api(libs.kotlin.mockito)
     api(libs.assertj)
-
-    api(libs.exposed.core)
-    api(libs.flyway)
-
-    api(libs.kafka.clients)
+    api(libs.kotlin.mockito)
 
     api(libs.ktor.server.tests.jvm)
     api(libs.ktor.server.test.host)
-    implementation(libs.ktor.server.content.negotiation)
 
     api(libs.testcontainers.bom)
     api(libs.testcontainers.junit)
@@ -30,5 +19,6 @@ dependencies {
     api(libs.testcontainers.postgresql)
     api(libs.testcontainers.jdbc)
     api(libs.testcontainers.kafka)
+
     api(libs.mockserver.client)
 }
