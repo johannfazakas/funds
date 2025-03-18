7efd3b7cbd4e44f5d6b481f1224ebd726166dfae
Johann Fazakas <johannfazakas@gmail.com>
2025-03-18 16:32:18 +0200

    small improvements

--- service/reporting/reporting-service/src/main/kotlin/ro/jf/funds/reporting/service/service/reportdata/resolver/GroupedBudgetDataResolver.kt
+++ service/reporting/reporting-service/src/main/kotlin/ro/jf/funds/reporting/service/service/reportdata/resolver/GroupedBudgetDataResolver.kt
@@ -4,9 +4,15 @@ import ro.jf.funds.reporting.service.domain.Budget
 import ro.jf.funds.reporting.service.domain.ByBucket
 import ro.jf.funds.reporting.service.domain.ByGroup
 import ro.jf.funds.reporting.service.domain.ByUnit
+import java.math.BigDecimal
 
 class GroupedBudgetDataResolver : ReportDataResolver<ByGroup<ByUnit<Budget>>> {
     override fun resolve(input: ReportDataResolverInput): ByBucket<ByGroup<ByUnit<Budget>>>? {
+        val previousLeftBudgets = calculatePreviousLeftBudgets()
         return null
+    }
+
+    private fun calculatePreviousLeftBudgets(): ByGroup<ByUnit<BigDecimal>> {
+        return ByGroup(emptyMap())
     }
 }
