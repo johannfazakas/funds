5b40a50bb1d190c68f050228ee57ae6d9eb99433
Johann Fazakas <johannfazakas@gmail.com>
2025-05-06 09:55:18 +0300

    resolve required conversions for grouped data

--- service/reporting/reporting-service/src/main/kotlin/ro/jf/funds/reporting/service/service/reportdata/ReportDataService.kt
+++ service/reporting/reporting-service/src/main/kotlin/ro/jf/funds/reporting/service/service/reportdata/ReportDataService.kt
@@ -2,6 +2,7 @@ package ro.jf.funds.reporting.service.service.reportdata
 
 import mu.KotlinLogging.logger
 import ro.jf.funds.commons.model.Currency
+import ro.jf.funds.commons.model.FinancialUnit
 import ro.jf.funds.historicalpricing.api.model.ConversionRequest
 import ro.jf.funds.historicalpricing.api.model.ConversionsRequest
 import ro.jf.funds.historicalpricing.api.model.ConversionsResponse
@@ -13,7 +14,10 @@ import ro.jf.funds.reporting.service.persistence.ReportViewRepository
 import ro.jf.funds.reporting.service.service.getBuckets
 import ro.jf.funds.reporting.service.service.reportdata.resolver.ReportDataResolverInput
 import ro.jf.funds.reporting.service.service.reportdata.resolver.ReportDataResolverRegistry
+import java.math.BigDecimal
 import java.util.*
+// TODO(Johann) measuring time could be done using prometheus maybe
+import kotlin.time.measureTimedValue
 
 private val log = logger { }
 
@@ -51,10 +55,10 @@ class ReportDataService(
     ): List<BucketData<ReportDataAggregate>> {
         val input = ReportDataResolverInput(granularInterval, catalog, conversions, reportDataConfiguration)
 
-        val netData = resolverRegistry.net.resolve(input)
-        val groupedNetData = resolverRegistry.groupedNet.resolve(input)
-        val valueReportData = resolverRegistry.valueReport.resolve(input)
-        val groupedBudgetData = resolverRegistry.groupedBudget.resolve(input)
+        val netData = resolveNetData(input)
+        val groupedNetData = resolveGroupedNetData(input)
+        val valueReportData = resolveValueReportData(input)
+        val groupedBudgetData = resolveGroupedBudgetData(input)
 
         return granularInterval.getBuckets()
             .map { bucket ->
@@ -76,24 +80,87 @@ class ReportDataService(
         targetUnit: Currency,
         reportRecords: List<ReportRecord>,
         granularInterval: GranularDateInterval,
-    ): ConversionsResponse {
+    ): ConversionsResponse = measureTimedValue {
         val sourceUnits = reportRecords
             .asSequence()
             .map { it.unit }
             .filter { it != targetUnit }
-            .distinct().toList()
+            .distinct()
+            .toList()
 
+        val conversionsRequest = sequenceOf(
+            getBucketBoundingConversionRequests(sourceUnits, targetUnit, granularInterval),
+            getRecordConversionRequests(sourceUnits, targetUnit, reportRecords)
+        )
+            .flatten().distinct().toList().let(::ConversionsRequest)
+        historicalPricingSdk.convert(userId, conversionsRequest)
+    }.let { (result, duration) ->
+        log.debug { "Conversions resolved in $duration" }
+        result
+    }
+
+    private fun getRecordConversionRequests(
+        sourceUnits: List<FinancialUnit>,
+        targetUnit: Currency,
+        reportRecords: List<ReportRecord>,
+    ): List<ConversionRequest> {
+        return reportRecords
+            .asSequence()
+            .map { record -> record.date }
+            .distinct()
+            .flatMap { date -> sourceUnits.map { sourceUnit -> ConversionRequest(sourceUnit, targetUnit, date) } }
+            .toList()
+    }
+
+    private fun getBucketBoundingConversionRequests(
+        sourceUnits: List<FinancialUnit>,
+        targetUnit: Currency,
+        granularInterval: GranularDateInterval,
+    ): List<ConversionRequest> {
         val dates = granularInterval.getBuckets().flatMap { listOf(it.from, it.to) }.toList()
 
-        val conversionsRequest = ConversionsRequest(
-            conversions = sourceUnits
-                .asSequence()
-                .flatMap { sourceUnit -> dates.map { sourceUnit to it } }
-                .map { (sourceUnit, date) ->
-                    ConversionRequest(sourceUnit, targetUnit, date)
-                }
-                .toList()
-        )
-        return historicalPricingSdk.convert(userId, conversionsRequest)
+        return sourceUnits
+            .asSequence()
+            .flatMap { sourceUnit -> dates.map { sourceUnit to it } }
+            .map { (sourceUnit, date) ->
+                ConversionRequest(sourceUnit, targetUnit, date)
+            }
+            .toList()
+    }
+
+    private fun resolveNetData(input: ReportDataResolverInput): ByBucket<BigDecimal>? {
+        val (netData, netDataDuration) =
+            measureTimedValue { resolverRegistry.net.resolve(input) }
+        if (netData != null) {
+            log.debug { "Net data resolved in $netDataDuration" }
+        }
+        return netData
+    }
+
+    private fun resolveGroupedNetData(input: ReportDataResolverInput): ByBucket<ByGroup<BigDecimal>>? {
+        val (groupedNetData, groupedNetDataDuration) =
+            measureTimedValue { resolverRegistry.groupedNet.resolve(input) }
+        if (groupedNetData != null) {
+            log.debug { "Grouped net data resolved in $groupedNetDataDuration" }
+        }
+        return groupedNetData
+    }
+
+    private fun resolveValueReportData(input: ReportDataResolverInput): ByBucket<ValueReport>? {
+        val (valueReportData, valueReportDataDuration) =
+            measureTimedValue { resolverRegistry.valueReport.resolve(input) }
+        if (valueReportData != null) {
+            log.debug { "Value report data resolved in $valueReportDataDuration" }
+        }
+        return valueReportData
+    }
+
+    private fun resolveGroupedBudgetData(input: ReportDataResolverInput): ByBucket<ByGroup<ByUnit<Budget>>>? {
+        val (groupedBudgetData, groupedBudgetDataDuration) =
+            measureTimedValue { resolverRegistry.groupedBudget.resolve(input) }
+        if (groupedBudgetData != null) {
+            log.debug { "Grouped budget data resolved in $groupedBudgetDataDuration" }
+        }
+        return groupedBudgetData
     }
 }
